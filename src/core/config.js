f29dc26e47a47444cc934f57f52c710191ae18a9
mile-ignjatovic <mileignjatovic.dev@gmail.com>
2023-02-26 20:04:46 +0100

    Initial commit

---
+++ src/core/config.js
@@ -0,0 +1,23 @@
+// Replace with your own firebase config!
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAYiuZaxArnAwPZzNCjFyirdTc2Zl0xyQs",
+  authDomain: "bet-ml-f1c3b.firebaseapp.com",
+  databaseURL: "https://bet-ml-f1c3b-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "bet-ml-f1c3b",
+  storageBucket: "bet-ml-f1c3b.appspot.com",
+  messagingSenderId: "268246168759",
+  appId: "1:268246168759:web:631254c9c073c5192ac932",
+  measurementId: "G-C31Y4YR6DB"
+}
+
+// Replace with your own IDs! follow the guides here:
+// https://docs.expo.io/versions/latest/sdk/google/#using-it-inside-of-the-expo-app
+export const ANDROID_GOOGLE_CLIENT_ID =
+  '878215484396-4pbcf3tghqe9sa0pvq3fifb5etnn4q6s.apps.googleusercontent.com'
+export const IOS_GOOGLE_CLIENT_ID =
+  '878215484396-5hiqfjetgvbdck03l0jepdgku6u64erm.apps.googleusercontent.com'
+
+// Replace with your own facebook app ID.
+// You can find more information how to generate one here:
+// https://docs.expo.io/versions/latest/sdk/facebook/#registering-your-app-with-facebook
+export const FACEBOOK_APP_ID = '820370405209413'
